5cb5c06f3b0fd9bd88388f43d8d3466969b49c26
amolina9-cell <amolina9@gmail.com>
2026-09-13 00:50:35 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
 export default defineConfig(() => {
   return {
     base: "/ieaas/",
+
     plugins: [react(), tailwindcss()],
 
     build: {
